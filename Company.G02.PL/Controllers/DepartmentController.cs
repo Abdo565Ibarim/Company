8990ba77101f0dc96253fdc2f0ce2bac09b10dbc
body <body@DESKTOP-78JB5FP>
2025-04-12 11:11:13 +0200

    Create Create (Action-View)

--- Company.G02.PL/Controllers/DepartmentController.cs
+++ Company.G02.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
-﻿using Company.GO2.BLL.Interfaces;
+﻿using Company.G02.PL.Dtos;
+using Company.GO2.BLL.Interfaces;
 using Company.GO2.BLL.Repositories;
+using Company.GO2.DAL.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.G02.PL.Controllers
@@ -18,6 +20,33 @@ namespace Company.G02.PL.Controllers
         {
             var departments = _departmentRepository.GetALL();
             return View(departments);
+        }
+
+        [HttpGet]
+        public IActionResult Create ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if(ModelState.IsValid)
+            {
+                var department = new Depratment()
+                {
+                    Code = model.Code,
+                    Name = model.Name,
+                    CreateAt = model.CreateAt,
+                };
+                 var conut = _departmentRepository.Add(department);
+                if(conut > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View(model);
         }
     }
 }
